1d28cf919c6d53cb3ef33db62c9d9344d19fa3bd
Whiz <diegofernandez29400@gmail.com>
2019-04-22 12:36:11 +0200

    Revert "c1"
    This reverts commit e5314d2f2afbe34b30fe87c61e04a3ebb82bce29.
    
    # Conflicts:
    #	src/clasestring/ClaseString.java

--- src/clasestring/ClaseString.java
+++ src/clasestring/ClaseString.java
@@ -14,9 +14,8 @@ public class ClaseString {
    
     public static void main(String[] args) {
         
-        //Cambio x
-        
-        String s = "diego.com";
+        String s = "yo.com";
+        // Hola 
       
         System.out.println("Tamaños: " + s.length());
         
@@ -26,7 +25,7 @@ public class ClaseString {
         // Obtener subcadena desde el caracter i-ésimo hasta el final
         // de la cadena
         String subcadena = s.substring(3);
-        System.out.println("SubString = " + subcadena);
+        System.out.println("Subcadena = " + subcadena);
        
         // Obtener subcadena desde el caracter i-ésimo hasta el 
         // caracter j-ésimo de la cadena
